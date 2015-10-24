cdb914867bb9652a8fd8fd6728e6447e78930343
Tyler J. Stachecki <tstache1@binghamton.edu>
2015-10-24 19:12:50 -0400

    Get the example working on cmips.
    Signed-off-by: Tyler J. Stachecki <tstache1@binghamton.edu>

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-mips-baremetal-elf-gcc -mno-check-zero-division -march=4kc -msoft-float -nostartfiles *.c *.S -o kernel
+mips-baremetal-elf-gcc -mno-check-zero-division -march=4kc -msoft-float -nostartfiles *.c *.S -o kernel -Wl,-Ttext,0x80001000
 mips-baremetal-elf-objcopy -Osrec kernel kernel.srec
